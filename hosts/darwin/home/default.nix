8770c3eb3b5db2007a654631951fd3b5bed787f6
Petre Tiberiu <tiberiu.petre@hotmail.com>
2025-08-21 18:44:22 +0300

    Now we can download wallpapers from the internet and apply them

--- hosts/darwin/home/default.nix
+++ hosts/darwin/home/default.nix
@@ -1,4 +1,4 @@
-{ self, lib, pkgs, username, homeDirectory, projectRoot, ... }:
+{ self, lib, pkgs, ... }:
 {
   ###################################################################################
   #  MacOS's Darwin Home Manager configuration
@@ -13,12 +13,12 @@
 
   # This module configures Home Manager for the user on macOS systems.
   # It allows you to manage user-specific configurations and packages using Home Manager.
-  home.username = username; # Set the username for Home Manager
-  home.homeDirectory = homeDirectory; # Set the home directory for Home Manager
+  home.username = "tiberiu"; # Set the username for Home Manager
+  home.homeDirectory = "/Users/tiberiu"; # Set the home directory for Home Manager
 
   # Set the Home-Manager state version; this should match the version of nix-darwin you are using.
   # Valid values can be found in the nix-darwin documentation: https://daiderd.com/nix-darwin/manual/index.html#opt-system.stateVersion
-  home.stateVersion = "25.11"; # Using the latest state version for macOS 26 (Tahoe)
+  home.stateVersion = "25.11"; # Using the state version for NixOS 25.11
 
   # Home Manager module for Darwin (macOS)
   # This module configures Home Manager for the user on macOS systems.
@@ -26,6 +26,7 @@
   home.packages = with pkgs; [
     # Add your desired packages here, e.g. git, zsh, etc.
     hello # A simple package to demonstrate Home Manager usage
+    curl # A command tool interface for transfering data from internet
   ];
 
   # Enable Home Manager for the user
@@ -43,7 +44,30 @@
   # and set it to the desired wallpaper path.
   programs.wallpaper = {
     enable = true; # Enable wallpaper management
-    image = "${projectRoot}/wallpaper.jpg"; # Path to the wallpaper image
+
+    # Set the path to the wallpaper image
+    # This is necessary for ensuring that the wallpaper is set correctly for the user
+    # and that the user has access to the wallpaper image.
+    # Replace with your actual wallpaper image path.
+    # For example, you can use a local image file or a remote URL.
+    # Note: The image path should be accessible to the user.
+    imagePath = "/Users/tiberiu/Pictures/Wallpapers/jgycezrufvif1.png"; # Path to the wallpaper image
+
+    # Set the URL to download the wallpaper image if it does not exist
+    # This is useful for ensuring that the wallpaper is set correctly for the user
+    # and that the user has access to the wallpaper image.
+    # If the image does not exist, the script will attempt to download it from the specified URL.
+    # If you want to use a default wallpaper, you can set this to a valid URL.
+    # For example, you can use a public image URL or a remote image file.
+    # If you leave this as null, the script will not attempt to download the image.
+    imageUrl = "https://i.redd.it/jgycezrufvif1.png";
+
+    # Set the display index for the wallpaper
+    # This is necessary for ensuring that the wallpaper is set correctly for the user
+    # and that the user has access to the correct display.
+    # If you have multiple displays, you can change the following line
+    # and set it to the desired display index.
+    # The display index is zero-based, so 0 refers to the primary screen.
     display = 0; # Display index (0 = primary screen)
   };
 }
--- hosts/darwin/home/wallpaper.nix
+++ hosts/darwin/home/wallpaper.nix
@@ -17,9 +17,15 @@ in {
   options.programs.wallpaper = {
     enable = lib.mkEnableOption "Set wallpaper using desktoppr";
 
-    image = lib.mkOption {
+    imagePath = lib.mkOption {
       type = lib.types.path;
       description = "Path to the wallpaper image";
+    };
+
+    imageUrl = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = "URL to download the wallpaper image if it does not exist";
     };
 
     display = lib.mkOption {
@@ -33,11 +39,19 @@ in {
     home.activation.wallpaper = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       echo "Setting wallpaper for display ${toString cfg.display}"
       ${pkgs.writeShellScriptBin "set-wallpaper" ''
-        # Ensure the image exists
-        if [ ! -f ${cfg.image} ]; then
-          echo "Wallpaper image not found: ${cfg.image}"
-          exit 1
-        fi
+		echo "Downloading the wallpaper from the provided url..."
+		# Check if imageUrl is provided
+		if [ -n "${cfg.imageUrl}" ]; then
+			# Use curl to download the wallpaper from the provided URL
+			# Ensure that the URL points to a valid image file
+			/usr/bin/curl -o ${cfg.imagePath} ${cfg.imageUrl}
+			if [ $? -eq 0 ]; then
+				echo "Default wallpaper downloaded successfully."
+			else
+				echo "Failed to download the default wallpaper from ${cfg.imageUrl}."
+				exit 1
+			fi
+		fi
 
         # Check if desktoppr command is available
         # Ensure that desktoppr is installed and available in the PATH
@@ -46,13 +60,13 @@ in {
           exit 1
         fi
 
-        echo "Setting wallpaper to ${cfg.image} on display ${toString cfg.display}"
+        echo "Setting wallpaper to ${cfg.imagePath} on display ${toString cfg.display}"
 
         # Set the wallpaper using desktoppr
         # The desktoppr command should be available in the PATH, or you can specify its full path
         # For example, if desktoppr is installed via Homebrew, it might be located at /opt/homebrew/bin/desktoppr
         # Adjust the path accordingly if necessary
-        /usr/local/bin/desktoppr ${toString cfg.display} ${cfg.image}
+        /usr/local/bin/desktoppr ${toString cfg.display} ${cfg.imagePath}
       ''}/bin/set-wallpaper
     '';
   };
